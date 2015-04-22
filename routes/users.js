c0e3fc91ab5e57801ca7980f7a2cb43826918a85
akshaybhasme <akshaybhasme@192.168.0.26>
2015-04-22 00:50:09 -0700

    added mongoose dependency and connected to database

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://admin:akshay@ds041871.mongolab.com:41871/cmpe281');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
