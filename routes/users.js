bff1d84aae1db5881a17412a666bb1d027a91fff
akshaybhasme <akshaybhasme@192.168.0.26>
2015-04-22 16:02:32 -0700

    auth apis done

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var crypto = require('crypto');
 
 mongoose.connect('mongodb://admin:akshay@ds041871.mongolab.com:41871/cmpe281');
+
+var User = mongoose.model('users', {name : String, email : String, pwHash : String});
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -14,20 +17,54 @@ var sess;
 router.get('/isLoggedIn', function(req, res){
 	sess = req.session;
 	if(sess.email){
-		res.send({loggedIn: true});
+		User.findOne({ email: sess.email}, function(err, user){
+			res.send(user);
+		});
 	}else{
-		res.send({loggedIn: false});
+		res.send({error: "Not logged in"});
 	}
 });
 
 router.post('/login', function(req, res){
 	sess = req.session;
-	console.log(req);
-	sess.email = req.body.email;
+	
+	var pwHashS = crypto.createHash('md5').update(req.body.password).digest('hex');
+	
+	User.findOne({ email: req.body.email, pwHash : pwHashS}, function(err, user){
+		if(user){
+			sess.email = user.email;
+			res.send(user);
+		}else{
+			res.send({error : "Credentials did not match any of our records"});
+		}
+	});
 });
 
 router.post('/register', function(req, res){
 	sess = req.session;
+	
+	var pwHashS = crypto.createHash('md5').update(req.body.password).digest('hex');
+	
+	//Find if user already exists with same email and password
+	User.findOne({ email: req.body.email}, function(err, user){
+		if(user){
+			if(user.pwHash === pwHashS){
+				sess.email = user.email;
+				res.send(user);
+			}else{
+				res.send({error : "User with email already exists"});
+			}
+		}else{
+			//Save user
+			var u = new User({email: req.body.email, pwHash: pwHashS, name : req.body.name});
+			u.save(function(err){
+				if(err) {throw err;}
+				sess.email = u.email;
+				res.send(u);
+			});
+		}
+	});
+	
 });
 
 module.exports = router;
