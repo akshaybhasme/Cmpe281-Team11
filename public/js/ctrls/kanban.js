db1bc3188764972d20a0e8e40f9e613ec4d6e2c6
Akshay Joshi <akshayj25cfc@gmail.com>
2015-05-01 13:27:00 -0700

    kanban

--- public/js/ctrls/kanban.js
+++ public/js/ctrls/kanban.js
@@ -1,111 +1,100 @@
 cmpe.controller('kanbanCtrl', function($scope, $stateParams, $log, $modal,
 		$timeout, $rootScope, $http) {
 
-	$scope.list1 = [ {
-		id : 3,
-		title : 'task3',
-		description : 'Desc three',
-		assigneduser : 'User1',
-		duration : 10,
-		status : 1,
-		drag : true
-	}, {
-		id : 4,
-		title : 'task4',
-		description : 'Desc four',
-		assigneduser : 'User2',
-		duration : 15,
-		status : 1,
-		drag : true
-	} ];
+	$scope.list1 = [];
 
-	$scope.list2 = [ {
-		id : 1,
-		title : 'task1',
-		description : 'Desc one',
-		assigneduser : 'User1',
-		duration : 12,
-		status : 2,
-		drag : true
-	}, {
-		id : 2,
-		title : 'task2',
-		description : 'Desc two',
-		assigneduser : 'User2',
-		duration : 11,
-		status : 2,
-		drag : true
-	} ];
+	$scope.list2 = [];
 
-	$scope.list3 = [ {
-		id : 5,
-		title : 'task5',
-		description : 'Desc five',
-		assigneduser : 'User1',
-		duration : 10,
-		status : 3,
-		drag : true
-	}, {
-		id : 6,
-		title : 'task6',
-		description : 'Desc six',
-		assigneduser : 'User2',
-		duration : 10,
-		status : 3,
-		drag : true
-	} ];
+	$scope.list3 = [];
 
 	$scope.title = "KANBAN Project";
 
 	$scope.getListing = function() {
-		$http.get('/api/listings/' + $scope.apartment.place_id).success(
+		$http.get('/api/getTasks/kanban/' + $stateParams.projectID).success(
 				function(data) {
-					$scope.data = [];
 					angular.forEach(data, function(v, i) {
-						if (v.status == 1) {
-							$scope.list1.push(v);
-						} else if (v.status == 2) {
-							$scope.list2.push(v);
+						if (v.object.status == "Requested") {
+							v.object.id = v._id;
+							$scope.list1.push(v.object);
+						} else if (v.object.status == "In Progress") {
+							v.object.id = v._id;
+							$scope.list2.push(v.object);
 						} else {
-							$scope.list3.push(v);
+							v.object.id = v._id;
+							$scope.list3.push(v.object);
 						}
 					});
 
 				});
 	};
-	
-	$scope.dropSuccessHandler= function($event,$index,list){
-		console.log(In);
+
+	$scope.dropSuccessHandler = function($event, $index, list) {
+		console.log(list);
 	}
-	
-	$scope.updateListing = function($event,$index,list) {
-		
-		console.log(list[$index]);
-		//$scope.list1.splice($scope.list1.indexOf(listing), 1);
+
+	$scope.updateListing1 = function($event, $index, list) {
+		list[$index].status="Requested";
+		var o = {
+				object : list[$index]
+			};
+		$http.put("/api/updateTask/" + list[$index].id, o)
+		.success(function(data)  {
+			console.log('Saved :' + data);
+		});
+	};
+	$scope.updateListing2 = function($event, $index, list) {
+		list[$index].status="In Progress";
+		var o = {
+				object : list[$index]
+			};
+		$http.put("/api/updateTask/" + list[$index].id, o)
+		.success(function(data)  {
+			console.log('Saved :' + data);
+		});
+	};
+	$scope.updateListing3 = function($event, $index, list) {
+		list[$index].status="Done";
+		var o = {
+				object : list[$index]
+			};
+		$http.put("/api/updateTask/" + list[$index].id, o)
+		.success(function(data)  {
+			console.log('Saved :' + data);
+		});
 	};
 
 	$scope.deleteListing1 = function(listing) {
-		$scope.list1.splice($scope.list1.indexOf(listing), 1);
-		/*
-		 * $http({ method: "post", url: "api/listings/delete/"+listing._id
-		 * }).success(function(data){ $scope.getListing(); });
-		 */
+		console.log(listing);
+
+		$http({
+			method : "delete",
+			url : "/api/deleteTask/" + listing.id
+		}).success(function(data) {
+			$scope.list1.splice($scope.list1.indexOf(listing), 1);
+		});
+
 	};
 
 	$scope.deleteListing2 = function(listing) {
-		$scope.list2.splice($scope.list2.indexOf(listing), 1);
-		/*
-		 * $http({ method: "post", url: "api/listings/delete/"+listing._id
-		 * }).success(function(data){ $scope.getListing(); });
-		 */
+
+		$http({
+			method : "delete",
+			url : "/api/deleteTask/" + listing.id
+		}).success(function(data) {
+			$scope.list2.splice($scope.list2.indexOf(listing), 1);
+		});
+
 	};
 
 	$scope.deleteListing3 = function(listing) {
-		$scope.list3.splice($scope.list3.indexOf(listing), 1);
-		/*
-		 * $http({ method: "post", url: "api/listings/delete/"+listing._id
-		 * }).success(function(data){ $scope.getListing(); });
-		 */
+
+		$http({
+			method : "delete",
+			url : "/api/deleteTask/" + listing.id
+		}).success(function(data) {
+			$scope.list3.splice($scope.list3.indexOf(listing), 1);
+		});
+
 	};
 
 	$scope.items = [];
@@ -121,27 +110,38 @@ cmpe.controller('kanbanCtrl', function($scope, $stateParams, $log, $modal,
 			}
 		});
 
-		modalInstance.result.then(function(selectedItem) {
-			if (selectedItem.status == 1)
-				$scope.list1.push(selectedItem);
-
-			else if (selectedItem.status == 2)
-				$scope.list2.push(selectedItem);
-
-			else if (selectedItem.status == 3)
-				$scope.list3.push(selectedItem);
-
+		modalInstance.result.then(function(selItem) {
+			var o = {
+				object : selItem
+			};
+			$http.post("/api/addTask/kanban/" + $stateParams.projectID, o)
+					.success(function(data) {
+						if (selItem.status == "Requested") {
+							selItem.id = data._id;
+							$scope.list1.push(selItem);
+						} else if (selItem.status == "In Progress") {
+							selItem.id = data._id;
+							$scope.list2.push(selItem);
+						} else if (selItem.status == "Done") {
+							selItem.id = data._id;
+							$scope.list3.push(selItem);
+						}
+					});
 		}, function() {
 			$log.info('Modal dismissed at: ' + new Date());
 		});
 	};
+	$scope.getListing();
 });
 
 cmpe.controller('modalKanbanCtrl', function($scope, $modalInstance) {
+	$scope.task = {};
 
+	$scope.task = {
+		status : "Requested"
+	}
+	$scope.projstatus = [ "Requested", "In Progress", "Done" ];
 	$scope.ok = function() {
-		// console.log($scope.task);
-		$scope.task.drag = true;
 		$modalInstance.close($scope.task);
 
 	};
